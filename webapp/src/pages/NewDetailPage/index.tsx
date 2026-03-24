363abc0d7f20649bf3802cb5b26a9e9e54ccc7df
Artur023 <artur.023@mail.ru>
2026-03-24 13:33:52 +0400

    Feat: add formik

--- webapp/src/pages/NewDetailPage/index.tsx
+++ webapp/src/pages/NewDetailPage/index.tsx
@@ -1,10 +1,34 @@
-import css from './index.module.scss';
+import { Segment } from '../../components/Segment';
+import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
+import { useFormik } from 'formik';
 
 export function NewDetailPage() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      nick: '',
+      description: '',
+      text: '',
+    },
+    onSubmit: (values) => {
+      console.info(values);
+    },
+  });
   return (
-    <div className={css.tittle}>
-      <h1>New Detail</h1>
-      <div className={css.content}> from will be here </div>
-    </div>
+    <Segment title={'New Detail'}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.info(formik.values);
+        }}
+      >
+        <Input name="name" label="Name" formik={formik} />
+        <Input name="nick" label="Nick" formik={formik} />
+        <Input name="description" label="Description" formik={formik} />
+        <Textarea name="text" label="Text" formik={formik} />
+        <button type="submit">Submit</button>
+      </form>
+    </Segment>
   );
 }
